a172ae2b761d0cdf8f9904642fd1c83d0c7f7c41
Andrey <perkussionist@gmail.com>
2016-10-22 17:29:40 +0300

    'Открытие_файла_из_определённой_директории'

--- open_file_from_directory.py
+++ open_file_from_directory.py
@@ -1,4 +1,8 @@
-with open("C:\\example\\text.txt", "r") as myfile: 
+import os
+
+path = os.path.join("/", "example", "text.txt")
+#Путь указываем в составном виде, чтобы он подходил для рахных ОС
+with open(path, "r") as myfile: 
 #Указать путь к файлу и режим работы с файлом
     print myfile.read()
     # обработка файла
