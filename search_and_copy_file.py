d5dbc042f46317f12c14250198566cc8a8591846
Andrey <perkussionist@gmail.com>
2016-10-26 20:45:00 +0300

    'Поиск_файла_и_его_копирование'

--- search_and_copy_file.py
+++ search_and_copy_file.py
@@ -9,4 +9,4 @@ for file in os.listdir(path):
     if fnmatch.fnmatch(file, 'OMG*'): #Поиск среди файлов текущей папки по маске
         full_path = os.path.join("/", path, file) #Создание полного пути копируемого файла
         new_path = os.path.join("/", "projects", "test2", file) #Создание полного пути нового копируемого файла
-        shutil.copyfile(full_path, new_path) #Выполнение копирования
+        shutil.copyfile(full_path, new_path) #Выполнение копирования файла
