9cc00e01d6b85653af18cdfe8895f1262161a57e
Andrey <perkussionist@gmail.com>
2016-10-27 21:44:21 +0300

    Парсинг_файла_ПЕРВОГО_типа

--- parsing1.py
+++ parsing1.py
@@ -1,6 +1,6 @@
 import os
 import fnmatch
-
+from collections import Counter
 path = os.path.join("/", "projects", "test2")
 #Путь указываем в составном виде, чтобы он подходил для рахных ОС
 for file in os.listdir(path):
@@ -9,5 +9,7 @@ for file in os.listdir(path):
 with open(full_path, 'r') as myfile: #Чтение файла построчно
     lines = myfile.readlines()
     data = lines[1:-1]
+    data_list = []
     for string in data:
-        print(string[272:274])
+        data_list.append(string[272:274])
+    values_count = Counter(data_list)
