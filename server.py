65279ec74125db3ac8ebe35fd84dad6e54daafb8
Andrey <perkussionist@gmail.com>
2016-10-27 21:43:46 +0300

    локальный_сервер

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from parsing1 import values_count
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Поднял локальный сервер на Flask"
+    #return "Поднял локальный сервер на Flask"
+    return values_count
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
