75dd62bfdd9b253e4c451006bd88ff58d86a89cd
Andrey <perkussionist@gmail.com>
2016-10-29 19:30:20 +0300

    Парсинг_файла_ВТОРОГО_типа

--- parsing2.py
+++ parsing2.py
@@ -7,8 +7,8 @@ for file in os.listdir(path):
     if fnmatch.fnmatch(file, 'MIN_DISIGN*'): #Поиск среди файлов текущей папки по маске
         full_path = os.path.join("/", path, file)
 with open(full_path, 'r', encoding='utf-8') as myfile: #Чтение файла построчно
-    lines = myfile.readlines()
+    lines = myfile.readlines() #Чтение файла построчно
     data_list = []
     for string in lines:
-        data_list.append(string[27:43].strip())
-    values_count2 = Counter(data_list)
+        data_list.append(string[27:43].strip())  #Извлекаем из каждой строки нужное значение и создаем список с этими значениями
+    values_count2 = Counter(data_list) #Счётчик количества одинаковых значений
