ca07b2847fe086e8bd231bbd7c69a24fd3b5ffbd
Andrey <perkussionist@gmail.com>
2016-10-29 19:28:03 +0300

    Парсинг_файла_ПЕРВОГО_типа

--- parsing2.py
+++ parsing2.py
@@ -6,10 +6,9 @@ path = os.path.join("/", "projects", "test2")
 for file in os.listdir(path):
     if fnmatch.fnmatch(file, 'MIN_DISIGN*'): #Поиск среди файлов текущей папки по маске
         full_path = os.path.join("/", path, file)
-with open(full_path, 'r') as myfile: #Чтение файла построчно
+with open(full_path, 'r', encoding='utf-8') as myfile: #Чтение файла построчно
     lines = myfile.readlines()
     data_list = []
     for string in lines:
         data_list.append(string[27:43].strip())
-    print(data_list)
-    values_count = Counter(data_list)
+    values_count2 = Counter(data_list)
