c2f84e3f876b8e27a5c7d15a8af9c287e269df60
Andrey <perkussionist@gmail.com>
2016-10-29 19:30:38 +0300

    Парсинг_файла_ТРЕТЬЕГО_типа

--- parsing3.py
+++ parsing3.py
@@ -4,34 +4,35 @@ from collections import Counter
 path = os.path.join("/", "projects", "test2")
 #Путь указываем в составном виде, чтобы он подходил для рахных ОС
 for file in os.listdir(path):
-    if fnmatch.fnmatch(file, 'OPT_LOCAL_MIFARE*'): #Поиск среди файлов текущей папки по маске
+    if fnmatch.fnmatch(file, 'TOP_LOCAL_MIFARE*'): #Поиск среди файлов текущей папки по маске
         full_path = os.path.join("/", path, file)
 with open(full_path, 'r') as myfile: #Чтение файла построчно
     lines = myfile.readlines()
+    data = {}
     if fnmatch.fnmatch(file, '*ShChPR*'):
-        pass
-        return len(lines)
+        count = len(lines)
+        data = {'ShChPR': count}
     elif fnmatch.fnmatch(file, '*Barnaul*'):
-        pass
-        return len(lines)
+        count = len(lines)
+        data = {'ShChPR': count}
     elif fnmatch.fnmatch(file, '*UlanUde*'):
-        pass
-        return len(lines)
+        count = len(lines)
+        data = {'Barnaul': count}
     elif fnmatch.fnmatch(file, '*Zhigulyovsk*'):
-        pass
-        return len(lines)
+        count = len(lines)
+        data = {'Zhigulyovsk': count}
     elif fnmatch.fnmatch(file, '*Novosibirsk*'):
-        pass
-        return len(lines)
+        count = len(lines)
+        data = {'Novosibirsk': count}
     elif fnmatch.fnmatch(file, '*Tolyaytti_pp*'):
-        pass
-        return len(lines)
-    elif fnmatch.fnmatch(file, '*Novoaltaisp_pp*'):
-        pass
-        return len(lines)
+        count = len(lines)
+        data = {'Tolyaytti_pp': count}
+    elif fnmatch.fnmatch(file, '*Novoaltaisk_pp*'):
+        count = len(lines)
+        data = {'Novoaltaisk_pp': count}
     elif fnmatch.fnmatch(file, '*Orenburg_pp*'):
-        pass
-        return len(lines)
+        count = len(lines)
+        data = {'Orenburg_pp': count}
     elif fnmatch.fnmatch(file, '*Arhangelsk*'):
-        pass
-        return len(lines)
+        count = len(lines)
+        data = {'Arhangelsk': count}
