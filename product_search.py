b2b2374d7073dec994f511c8ddbe490634dbfca7
Andrey <perkussionist@gmail.com>
2016-10-31 22:14:03 +0300

    Поиск_продуктов_базе_по_значениям

---
+++ product_search.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine
+from products_db import Product
+
+engine = create_engine('sqlite:///data.sqlite', echo=True)
+
+db_session = scoped_session(sessionmaker(bind=engine))
+
+def search1(code):
+    for product_name, in db_session.query(Product.product_name).filter_by(product_code=code)[:1]: 
+        return product_name
+
+def search2(code):
+    for special_code, in db_session.query(Product.special_code).filter_by(special_code=code)[:1]: 
+        return special_code
+
+def search3(code):
+    for product_name, in db_session.query(Product.product_name).filter_by(city_code=code)[:1]: 
+        return product_name
