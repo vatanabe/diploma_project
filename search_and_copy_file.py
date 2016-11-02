acec4fe5b1a5c8d59c53eaad47513576706593dd
Andrey <perkussionist@gmail.com>
2016-11-02 23:12:05 +0300

    обновлено

--- search_and_copy_file.py
+++ search_and_copy_file.py
@@ -1,12 +1,18 @@
 import os
 import shutil
 import fnmatch
+import time
 
 path = os.path.join("/", "projects", "test")
 #Путь указываем в составном виде, чтобы он подходил для рахных ОС
 
-for file in os.listdir(path):
-    if fnmatch.fnmatch(file, 'OMG*'): #Поиск среди файлов текущей папки по маске
-        full_path = os.path.join("/", path, file) #Создание полного пути копируемого файла
-        new_path = os.path.join("/", "projects", "test2", file) #Создание полного пути нового копируемого файла
-        shutil.copyfile(full_path, new_path) #Выполнение копирования файла
+def copy_file():
+    for file in os.listdir(path):
+        if fnmatch.fnmatch(file, 'OMG*'): #Поиск среди файлов текущей папки по маске
+            full_path = os.path.join("/", path, file) #Создание полного пути копируемого файла
+            new_path = os.path.join("/", "projects", "test2", file) #Создание полного пути нового копируемого файла
+            shutil.copyfile(full_path, new_path) #Выполнение копирования файла
+
+while True:
+    copy_file()
+    time.sleep(5)
