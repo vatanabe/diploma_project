67a05d5ab4ee9adc53bf3ed077970a8a1874de50
Andrey <perkussionist@gmail.com>
2016-11-09 22:17:05 +0300

    Остаток_продукта_на_складе

--- product_amount.py
+++ product_amount.py
@@ -7,16 +7,24 @@ engine = create_engine('sqlite:///data.sqlite', echo=True)
 db_session = scoped_session(sessionmaker(bind=engine))
 
 def amount1(code):
-    for (id,) in db_session.query(Product.id).filter_by(product_code=code):#, used=0, special_design=0): 
-        for product_id, in db_session.query(Storage.product_id).filter_by(product_id=id):
-            print(id)
+    total_product_quantity = 0
+    for (id,) in db_session.query(Product.id).filter_by(product_code=code):
+        for (prod_chip,) in db_session.query(Product.prod_chip).filter_by(product_code=code):
+            for (prod_vendor,) in db_session.query(Product.prod_vendor).filter_by(product_code=code):
+                for (product_quantity,) in db_session.query(Storage.product_quantity).filter_by(product_type='new', product_id=id, chip=prod_chip, vendor=prod_vendor):
+                    total_product_quantity += product_quantity
+    return total_product_quantity
 
 def amount2(code):
-    for (id,), (prod_chip,), (prod_vendor,) in db_session.query(Product.id, Product.prod_chip, Product.prod_vendor).filter_by(special_code=code):
-        for (product_quantity,) in db_session.query(Storage.product_quantity).filter_by(product_type='colored', product_id=id, chip=prod_chip, vendor=prod_vendor):
-            return product_quantity
+    for (id,) in db_session.query(Product.id).filter_by(special_code=code):
+        for (prod_chip,) in db_session.query(Product.prod_chip).filter_by(special_code=code):
+            for (prod_vendor,) in db_session.query(Product.prod_vendor).filter_by(special_code=code):
+                for (product_quantity,) in db_session.query(Storage.product_quantity).filter_by(product_type='colored', product_id=id, chip=prod_chip, vendor=prod_vendor):
+                    return product_quantity
     return 0
 
 def amount3(code):
-    for product_name, in db_session.query(Product.product_name).filter_by(city_code=code)[:1]: 
-        return product_name
+    for (id,) in db_session.query(Product.id).filter_by(city_code=code):
+        for (product_quantity,) in db_session.query(Storage.product_quantity).filter_by(product_type='colored', product_id=id):
+            return product_quantity
+    return 0
