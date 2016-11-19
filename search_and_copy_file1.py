b181c7dc4f1e664fb39feaea3402aa42666c0267
Andrey <perkussionist@gmail.com>
2016-11-19 18:15:35 +0300

    Запись_в_базу_заявки_и_её_состава

--- search_and_copy_file1.py
+++ search_and_copy_file1.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import fnmatch
 import time
-from products_db import Action, InputFile, ProductInFile
+from products_db import Action, InputFile, ProductInFile, Product
 from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, ForeignKey
 from sqlalchemy.orm import scoped_session, sessionmaker
 from datetime import datetime
@@ -13,15 +13,17 @@ from product_amount import amount1, amount2, amount3
 path = os.path.join("/", "projects", "test")
 #Путь указываем в составном виде, чтобы он подходил для рахных ОС
 
-engine = create_engine('sqlite:///data.sqlite', echo=True)
+engine = create_engine('sqlite:///data.sqlite')
 
 db_session = scoped_session(sessionmaker(bind=engine))
 
-filenames = []
+def get_filenames():
+    filenames = []
 
-for file in os.listdir(path):
-    if fnmatch.fnmatch(file, 'OMG*'):
-        filenames.append(file)
+    for file in os.listdir(path):
+        if fnmatch.fnmatch(file, 'OMG*'):
+            filenames.append(file)
+    return filenames
 
 def copy_file():
     for file in os.listdir(path):
@@ -30,7 +32,7 @@ def copy_file():
             new_path = os.path.join("/", "projects", "test2", file) #Создание полного пути нового копируемого файла
             shutil.copyfile(full_path, new_path) #Выполнение копирования файла
 
-def add_action():
+def add_action(filenames):
     for filename in filenames:
         action = Action(action_datetime = datetime.now(), action_type  = "file_copy", input_file_name = filename, produced_by = 'bot')
         count = 0      
@@ -48,7 +50,7 @@ def add_action():
         else:
             print("else2")
 
-def add_input_file():
+def add_input_file(filenames):
     for filename in filenames:
         if "OMG" in filename:
             file_type = "OMG"
@@ -56,37 +58,31 @@ def add_input_file():
             file_type = "MIN_DESIGN"
         elif "TOP_LOCAL" in filename:
             file_type = "TOP_LOCAL_MIFARE"
-        input_file = InputFile(input_file_name = filename, input_file_type = file_type, action_file_time = datetime.now(), file_status = "started")
-        count = 0      
-        for (input_file_name,) in db_session.query(InputFile.input_file_name):
-            if input_file_name == filename:
-                print("replay")
-                count += 1     
-            else:
-                print("else")
-        if count == 0:
+
+        if db_session.query(InputFile.input_file_name).filter(InputFile.input_file_name == filename).count() > 0:
+            print("replay")    
+        else:
+            print("else")
             copy_file()
+            input_file = InputFile(input_file_name = filename, input_file_type = file_type, action_file_time = datetime.now(), file_status = "started")
             db_session.add(input_file)
             db_session.commit()
+
             if "OMG" in filename:
-                for name in values_count1:
-                    product = db_session.query(Product.product_name).filter_by(product_code=name)
-                    input_quantity = values_count1[name]
-                    product_in_file = ProductInFile(product_id = product.id, input_quantity, reject_quantity = 0, produced_quantity = 0, 
-                product_in_file_status = "started", input_file_id = input_file.id)
-                #time.sleep(10)
-        else:
-            print("else2")
+                for code in values_count1:
+                    product = Product
+                    product = product.query.filter_by(product_code=code).first()
+                    print(product)
+                    input_quantity = values_count1[code]
+                    product_in_file = ProductInFile(product_id = product.id, input_quantity = input_quantity, reject_quantity = 0, produced_quantity = 0, 
+                    product_in_file_status = "started", input_file_id = input_file.id)
+                    db_session.add(product_in_file)
+                    db_session.commit()
 
-def add_product_in_input_file():
-    for name in values_count1:
 
-    product_in_file = ProductInFile(product_id = "???", input_quantity, reject_quantity = 0, produced_quantity = 0, 
-        product_in_file_status = "started", input_file_id = '???')
-    db_session.add(product_in_file)
-    db_session.commit()
-
-while True:
-    add_action()
-    add_input_file()
-    time.sleep(10)
+if __name__ == "__main__":
+    while True:
+        filenames = get_filenames()
+        add_action(filenames)
+        add_input_file(filenames)
+        time.sleep(10)
