4334ff095e4f665eab1f5c8680b7a5258897a681
Andrey <perkussionist@gmail.com>
2016-11-19 15:30:12 +0300

    Подсветка

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ def first():
     result = "<head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' \
     content='IE=edge'><meta name='viewport' content='width=device-width, \
     initial-scale=1'><link href='static/css/bootstrap.min.css' rel='stylesheet'>\
-    </head><body><!DOCTYPE html><html lang='en'><table class='table table-bordered'>\
+    </head><body><!DOCTYPE html><html lang='en'><table class='table table-bordered table-hover'>\
     <tr><th>Продукт</th><th>Начальное количество</th><th>Итоговое количество</th>\
     <th>Брак</th><th>На складе</th><th>Наличие</th><th>Готовность</th></tr>"
     for name in values_count1:
@@ -31,8 +31,9 @@ def first():
         <td><div class='col-xs-6'><input type='number' class='form-control' placeholder='Введите..'></div></td>\
         <td>%s</td>\
         <td>%s</td>\
+        <td><p><button type='button' class='btn btn-primary btn-sm'>Готово</button></p></td>\
         </tr>" % (search1(name), values_count1[name], amount1(name), availability)
-    result += "</table><body></html>"
+    result += "</table></body></html>"
     return result
 
 @app.route("/2")
@@ -40,7 +41,7 @@ def second():
     result = "<head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' \
     content='IE=edge'><meta name='viewport' content='width=device-width, \
     initial-scale=1'><link href='static/css/bootstrap.min.css' rel='stylesheet'>\
-    </head><body><!DOCTYPE html><html lang='en'><table class='table table-bordered'>\
+    </head><body><!DOCTYPE html><html lang='en'><table class='table table-bordered table-hover'>\
     <tr><th>Продукт</th><th>Начальное количество</th><th>Итоговое количество</th>\
     <th>Брак</th><th>На складе</th><th>Наличие</th><th>Готовность</th></tr>"
     for name in values_count2:
@@ -57,7 +58,7 @@ def second():
         <td>%s</td>\
         <td><p><button type='button' class='btn btn-primary btn-sm'>Готово</button></p></td>\
         </tr>" % (search2(name), values_count2[name], amount2(name), availability)
-    result += "</table><body></html>"
+    result += "</table></body></html>"
     return result
 
 @app.route("/3")
@@ -65,7 +66,7 @@ def third():
     result = "<head><meta charset='utf-8'><meta http-equiv='X-UA-Compatible' \
     content='IE=edge'><meta name='viewport' content='width=device-width, \
     initial-scale=1'><link href='static/css/bootstrap.min.css' rel='stylesheet'>\
-    </head><body><!DOCTYPE html><html lang='en'><table class='table table-bordered'>\
+    </head><body><!DOCTYPE html><html lang='en'><table class='table table-bordered table-hover'>\
     <tr><th>Продукт</th><th>Начальное количество</th><th>Итоговое количество</th>\
     <th>Брак</th><th>На складе</th><th>Наличие</th><th>Готовность</th></tr>"
     for name in data:
@@ -80,8 +81,9 @@ def third():
         <td><div class='col-xs-6'><input type='number' class='form-control' placeholder='Введите..'></div></td>\
         <td>%s</td>\
         <td>%s</td>\
+        <td><p><button type='button' class='btn btn-primary btn-sm'>Готово</button></p></td>\
         </tr>" % (search3(name), data[name], amount3(name), availability)
-    result += "</table><body></html>"
+    result += "</table></body></html>"
     return result
 
 if __name__ == "__main__":
